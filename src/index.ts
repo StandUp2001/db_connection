c7036d3c74b01a7f14938c1afeab09ed7f7b800c
Stan van Dijk <536076@student.saxion.nl>
2023-12-16 22:26:02 +0100

    v0.0.6

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,5 @@
-import { FetchDB } from "./fetch";
-import { Column, getBodyFromTable } from "./utils";
+import { pathApi } from "./path";
+import { Column } from "./utils";
+import { getBodyFromTable, getTablesInfo } from "./utils";
 
-
-/**
- * Handles the routing logic for different REST API endpoints.
- * @param {D1Database} DB - The database object.
- * @param {string} table - The name of the table.
- * @param {string} pathname - The pathname of the request URL.
- * @returns {Promise<Response>} - The response from the corresponding database operation.
- */
-async function pathApi<T>(DB: D1Database, table: string, pathname: string, body: T): Promise<Response> {
-    const db = new FetchDB<T>(DB, table, body);
-    const restPathname = pathname.substring(table.length + 1);
-    switch (restPathname) {
-        case "/":
-        case "":
-            return await db.get();
-        case "add":
-        case "/add":
-        case "/add/":
-            return await db.add();
-        case "put":
-        case "/put":
-        case "/put/":
-            return await db.update();
-        case "delete":
-        case "/delete":
-        case "/delete/":
-            return await db.delete();
-        default:
-            return new Response(`Not found: ${restPathname}`, { status: 404 });
-    }
-}
-
-export { pathApi, Column, getBodyFromTable };
+export { pathApi, getBodyFromTable, getTablesInfo, Column };
